e42eacc54b19a6176311ce851a2828a6c0bb5bb0
AVIRAL MAHESHWARI <techaviral@gmail.com>
2025-08-26 22:25:33 +0530

    Configure Cloudinary and Start Multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_API,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on Cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file has been uploaded successfully
+    console.log("file is uploaded in cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as upload operation got failed
+    return null;
+  }
+};
